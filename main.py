1560a6152d1a1bbdd6c3741db905e0efb10728c4
astep2023 <astephens12319@gmail.com>
2022-03-26 12:47:08 -0500

    Update main.py
    can't remember exactly what i did here...

--- main.py
+++ main.py
@@ -12,54 +12,61 @@ running = True
 #This section is meant to display the time. I have it checking to see if countdown is still alive and if it is, then it'll do something. I honestly can't remember what tf I was thinking when I started that, but that is how it is for now. I'll probably go ahead and change it later cause looking back at it after not for a few days, that shit looks wack.
 
 def signal_handler(sig, frame):
+    global running
     print('You pressed Ctrl+C!')
     running = False
 
 def display_time():    
+    global running
     while running:
         date_time = datetime.now().strftime("%H:%M:%S")
         print(date_time)
         time.sleep(1)
+    
 #-----------------------------------#
 def countdown(seconds):
-    while seconds >= 0:
-        seconds -= 1
-        time.sleep(1)
-    print("Done")
-    #winsound.Beep(3000, 1000)
+    global test
+    global running
+    while running:
+        while seconds >= 0 and running:
+            seconds -= 1
+            time.sleep(1)
+        print("Done")
+        test.time_set()
+    
+    winsound.Beep(3000, 1000)
 #-----------------------------------#
 class alarm_clock():
     def __init__(self):
         self.time_set()
     
     def time_set(self):
-        self.seconds = int(input("How many seconds: "))
+        try: 
+            self.seconds = int(input("How many seconds: "))
+        except:
+            sys.exit(0)
     # This run method honestly looks terrible. I hate it. But this was like the messy and brute way I got things to display. Pretty sure the try and except stuff can be removed and this run method can be cut down a lot and the work can be put in other methods most likely.
 
     # So realized the try and except is actually still needed. The try and except is there to not recreate a thread_display thread. So a new thread isn't created each time you make a new timer/count_down.
     def run(self):
         if self.seconds <= -1:
-            quit()
+            sys.exit(0)
         else:
             while self.seconds >= 0:
-                try:
-                    if not thread_display.is_alive:
-                        thread_display.start()
-                except:
-                    thread_display = threading.Thread(target=display_time)
-                    thread_display.start()
+                thread_display = threading.Thread(target=display_time)
                 thread_countdown = threading.Thread(target=countdown, args=(self.seconds,))
+                thread_display.start()
                 thread_countdown.start()
                 thread_countdown.join()
                 thread_display.join()
-                self.time_set()
 
 #-----------------------------------#
 if __name__ == "__main__":
+    
     test = alarm_clock()
     test.run()
     signal.signal(signal.SIGINT, signal_handler)
-    #print('Press Ctrl+C')
+    
 #-----------------------------------#
 
 #This section below is just the original. It can be ignored overall.
